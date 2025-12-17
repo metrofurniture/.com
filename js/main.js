644d8ebe9ac9a8ce7b2933a2f273047732ab3bb6
metrofurniture <metro.furniture.mirpur@gmail.com>
2025-12-17 20:45:34 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -173,6 +173,17 @@
         }
     });
 
+	if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((registration) => {
+                console.log('Service worker registered with scope:', registration.scope);
+            })
+            .catch((error) => {
+                console.error('Service worker registration failed:', error);
+            });
+    });
+}
     /*------------------
         Counter
     --------------------*/

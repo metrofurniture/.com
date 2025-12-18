d01acb7471d0a77b014340885821beffe05076b7
metrofurniture <metro.furniture.mirpur@gmail.com>
2025-12-18 06:12:46 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*  ---------------------------------------------------
     Theme Name: Staging
-    Description: Staging bootstrap template
+    Description: Staging bootstrap tamplate
     Author: Colorib
     Author URI: https://www.colorib.com/
     Version: 1.0
@@ -38,9 +38,10 @@
         $(".offcanvas-menu-overlay").removeClass("active");
     });
 
+
     /*------------------
-        Navigation
-    --------------------*/
+		Navigation
+	--------------------*/
     $(".mobile-menu").slicknav({
         prependTo: '#mobile-menu-wrap',
         allowParentLinks: true
@@ -55,7 +56,7 @@
         items: 1,
         dots: true,
         nav: true,
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        navText: ["<i class='fa fa-angle-left'><i/>", "<i class='fa fa-angle-right'><i/>"],
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         smartSpeed: 1500,
@@ -65,21 +66,17 @@
         onInitialized: function (e) {
             var a = this.items().length;
             $("#snh-1").html("<span>01</span><span>" + "0" + a + "</span>");
-            var percentage = Math.round((100 / a));
-            $('.slider__progress span').css("width", percentage + "%");
+            var presentage = Math.round((100 / a));
+            $('.slider__progress span').css("width", presentage + "%");
         }
     }).on("changed.owl.carousel", function (e) {
         var b = --e.item.index,
             a = e.item.count;
-
-        // Change only backward and skip the second item
-        var displayedIndex = (b % 2 === 0) ? b : b === 1 ? 0 : 2; // Show 0 or 2 in the UI
-
-        $("#snh-1").html("<span>0" + (displayedIndex + 1) + "</span><span>0" + a + "</span>");
+        $("#snh-1").html("<span> " + "0" + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + "0" + a + "</span>");
 
         var current = e.page.index + 1;
-        var percentage = Math.round((100 / e.page.count) * current);
-        $('.slider__progress span').css("width", percentage + "%");
+        var presentage = Math.round((100 / e.page.count) * current);
+        $('.slider__progress span').css("width", presentage + "%");
     });
 
     /*--------------------------
@@ -91,7 +88,7 @@
         items: 4,
         dots: false,
         nav: true,
-        navText: ["<span class='arrow_carrot-left'></span>", "<span class='arrow_carrot-right'></span>"],
+        navText: ["<span class='arrow_carrot-left'><span/>", "<span class='arrow_carrot-right'><span/>"],
         smartSpeed: 1200,
         autoHeight: false,
         autoplay: false,
@@ -110,7 +107,7 @@
             }
         }
     });
-})(jQuery);
+
     /*-----------------------------
         Testimonial Slider
     -------------------------------*/

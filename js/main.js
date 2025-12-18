76f347ba53f34e84d49c7d58525dfeceffa30cc7
metrofurniture <metro.furniture.mirpur@gmail.com>
2025-12-18 06:01:29 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /*  ---------------------------------------------------
     Theme Name: Staging
-    Description: Staging bootstrap tamplate
+    Description: Staging bootstrap template
     Author: Colorib
     Author URI: https://www.colorib.com/
     Version: 1.0
@@ -37,10 +37,6 @@
         $(".offcanvas-menu-wrapper").removeClass("active");
         $(".offcanvas-menu-overlay").removeClass("active");
     });
-$(".offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("active");
-        $(".offcanvas-menu-overlay").removeClass("active");
-    });
 
     /*------------------
 		Navigation
@@ -59,7 +55,7 @@ $(".offcanvas-menu-overlay").on('click', function () {
         items: 1,
         dots: true,
         nav: true,
-        navText: ["<i class='fa fa-angle-left'><i/>", "<i class='fa fa-angle-right'><i/>"],
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         smartSpeed: 1500,
@@ -69,17 +65,20 @@ $(".offcanvas-menu-overlay").on('click', function () {
         onInitialized: function (e) {
             var a = this.items().length;
             $("#snh-1").html("<span>01</span><span>" + "0" + a + "</span>");
-            var presentage = Math.round((100 / a));
-            $('.slider__progress span').css("width", presentage + "%");
+            var percentage = Math.round((100 / a));
+            $('.slider__progress span').css("width", percentage + "%");
         }
     }).on("changed.owl.carousel", function (e) {
         var b = --e.item.index,
             a = e.item.count;
-        $("#snh-1").html("<span> " + "0" + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + "0" + a + "</span>");
+        
+        // Skip the second item
+        var displayedItem = (b + 1 === 2) ? 3 : b + 1; // Adjust index for skipped item
+        $("#snh-1").html("<span>0" + displayedItem + "</span><span>0" + a + "</span>");
 
         var current = e.page.index + 1;
-        var presentage = Math.round((100 / e.page.count) * current);
-        $('.slider__progress span').css("width", presentage + "%");
+        var percentage = Math.round((100 / e.page.count) * current);
+        $('.slider__progress span').css("width", percentage + "%");
     });
 
     /*--------------------------
@@ -91,7 +90,7 @@ $(".offcanvas-menu-overlay").on('click', function () {
         items: 4,
         dots: false,
         nav: true,
-        navText: ["<span class='arrow_carrot-left'><span/>", "<span class='arrow_carrot-right'><span/>"],
+        navText: ["<span class='arrow_carrot-left'></span>", "<span class='arrow_carrot-right'></span>"],
         smartSpeed: 1200,
         autoHeight: false,
         autoplay: false,
@@ -110,7 +109,7 @@ $(".offcanvas-menu-overlay").on('click', function () {
             }
         }
     });
-
+})(jQuery);
     /*-----------------------------
         Testimonial Slider
     -------------------------------*/
